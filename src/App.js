1655f8fded0859deb980ac53b917ff60df3bb9b9
LeoHCN <leo.cruznogueira@hotmail.com>
2024-11-10 01:05:03 -0300

    Criação das rotas de acordo com os conceitos apresentados em aula

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import TelaCadastroProduto from "./Telas/TelaCadastroProduto.jsx";
 import TelaCadastroCliente from "./Telas/TelaCadastroCliente.jsx";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Tela404 from "./Telas/Tela404.jsx";
+import TelaMenu from "./Telas/TelaMenu.jsx";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<TelaCadastroProduto />}></Route>
-          <Route path="/" element={<TelaCadastroCliente />}></Route>
+          <Route path="/" exact element={<TelaMenu/>}></Route>
+          <Route path="/cadastroProdutos" element={<TelaCadastroProduto />}></Route>
+          <Route path="/cadastroClientes" element={<TelaCadastroCliente />}></Route>
+          <Route path="*" element={<Tela404/>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/templates/Menu.jsx
+++ src/templates/Menu.jsx
@@ -1,17 +1,18 @@
 import {Navbar, Container, Nav, NavDropdown} from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 
 export default function Menu(props){
   return(
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Menu</Navbar.Brand>
+        <Navbar.Brand href="#" as={Link} to={"/"}>Menu</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <NavDropdown title="Cadastros" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.2">Produtos</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Clientes</NavDropdown.Item>
+              <NavDropdown.Item href="#" as={Link} to={"/cadastroProdutos"}>Produtos</NavDropdown.Item>
+              <NavDropdown.Item href="#" as={Link} to={"/cadastroClientes"}>Clientes</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
