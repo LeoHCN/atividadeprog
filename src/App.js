de677bc71ac47ee9141f6851a665d35b1a0c9658
LeoHCN <leo.cruznogueira@hotmail.com>
2024-11-10 00:19:56 -0300

    alterações de rotas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import TelaCadastroProduto from "./Telas/TelaCadastroProduto.jsx";
 import TelaCadastroCliente from "./Telas/TelaCadastroCliente.jsx";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <TelaCadastroProduto/>
-      <TelaCadastroCliente/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<TelaCadastroProduto />}></Route>
+          <Route path="/" element={<TelaCadastroCliente />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
